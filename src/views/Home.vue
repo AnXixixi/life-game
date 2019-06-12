ca51563447e24eda5c2fd69224801e81c4a5cf58
rivalove <542763565@qq.com>
2019-06-12 16:25:03 +0800

    页面编写

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,23 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+    <div class="home">
+        <div v-for="row in board">
+            <div v-for="col in row" style="">{{col}}</div>
+        </div>
+    </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
 
-export default {
-  name: 'home',
-  components: {
-    HelloWorld
+  export default {
+    data() {
+      return {
+        board: [
+          [0, 1, 0],
+          [0, 0, 1],
+          [1, 1, 1],
+          [0, 0, 0]
+        ]
+      }
+    }
   }
-}
 </script>
