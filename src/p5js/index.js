6e725b51b41b03133e93103d51d9b6000c53ae24
rivalove <542763565@qq.com>
2019-06-13 01:31:28 +0800

    页面逻辑写完，但是有点错误，需要再改改

--- src/p5js/index.js
+++ src/p5js/index.js
@@ -74,7 +74,7 @@ const init = () => {
     init()
     redraw()
   }
-
+  // 生成随机数 0 1 
   const randomRect = () => {
     if (!paused) {
       paused = true
@@ -86,23 +86,59 @@ const init = () => {
     }
     redraw()
   }
+}
 
-  // 画图函数，和 python 的 matplotlib 类似
-  const draw = () => {
-    background(255)
-    for (let i = 0; i < 24; i++) {
-      for (let j = 0; j < 36; j++) {
-        if (board[i][j] === 1) {
-          fill(0)
-        } else {
-          fill(255)
-        }
-        stroke(0)
-        rect(j * 20, i * 20, 19, 19)
+// 画图函数，和 python 的 matplotlib 类似
+const draw = () => {
+  background(255)
+  for (let i = 0; i < 24; i++) {
+    for (let j = 0; j < 36; j++) {
+      if (board[i][j] === 1) {
+        fill(0)
+      } else {
+        fill(255)
       }
+      stroke(0)
+      rect(j * 20, i * 20, 19, 19)
     }
-    if (!paused) {
-      board = gameOfLife(board)
-    }
-
   }
+  if (!paused) {
+    board = gameOfLife(board)
+    statusBox.html("Running")
+    statusBox.class('status-label running')
+  } else {
+    statusBox.html('Paused')
+    statusBox.class('status-label paused')
+  }
+  const rate = rateButton.value()
+  frameRate(rate)
+  rateButtonLabel.html(`frame rate: ${paused ? 0 : fr} fps`)
+
+}
+
+const doLoop = () => {
+  if (paused) {
+    loop()
+    paused = false
+  } else {
+    npLoop()
+    paused = true
+  }
+}
+
+const mouseClicked = () => {
+  const i = floor(mouseY / 20);
+  const j = floor(mouseX / 20);
+  if (j >= 36 || i >= 24 || i < 0 || j < 0 || !paused) {
+    return;
+  }
+  board[i][j] = board[i][j] === 1 ? 0 : 1;
+  redraw();
+}
+
+const keyPressed = () => {
+  if (keyCode == 32) {
+    handleLoop()
+    return false; // prevent default
+  }
+}
